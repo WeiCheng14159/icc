da8dde9ea0f3f5f5cba7fa7b236009363d3d45ba
cicero884 <joy861225@gmail.com>
2022-01-24 07:37:04 +0800

    add 2015 pre edited makefile for multiple make case and add -r on copy sim/dat

---
+++ graduate/icc2015cb_pre/src/SET.v
@@ -0,0 +1,16 @@
+module SET ( clk , rst, en, central, radius, mode, busy, valid, candidate );
+
+input clk, rst;
+input en;
+input [23:0] central;
+input [11:0] radius;
+input [1:0] mode;
+output busy;
+output reg valid;
+output reg [7:0] candidate;
+
+////////////////////////////////
+////////Define Somethings///////
+////////////////////////////////
+
+endmodule
