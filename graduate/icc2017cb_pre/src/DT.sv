26a06fe620638673ce4648ff2d55ad9c33cbcc8f
cicero884 <joy861225@gmail.com>
2022-02-18 14:26:06 +0800

    rewrite 2017pre

--- graduate/icc2017cb_pre/src/DT.sv
+++ graduate/icc2017cb_pre/src/DT.sv
@@ -14,31 +14,26 @@ module DT(
 	input		[7:0]	res_di
 );
 function [7:0] minimum;
-	input [7:0]in[5];
+	input [7:0]in[4];
 	int i;
 	begin
-		minimum=in[4];
-		for(i=0;i<4;++i) begin
+		minimum=in[0];
+		for(i=1;i<4;++i) begin
 			if(in[i]<minimum) minimum=in[i];
 		end
 	end
 endfunction
 
 //logic [13:0]core_cnt;
-logic [3:0] tmp_cnt;
+logic [1:0] tmp_cnt;
 logic [2:0] sti_cnt;
 logic [6:0] row_cnt;
 logic req_cnt;
-logic [7:0]sti_tmp[2][18];
-enum {INIT,JOIN_UP,JOIN_BOTTOM,CTRL,END} status;
+logic [1:0]req_cnt2;
+logic [7:0]sti_tmp[2][18],tmp;
+enum {GET_STI_DATA,JOIN_UP,JOIN_BOTTOM,GET_RES_DATA,END} status;
 int i;
-logic [7:0]tmp;
-
-logic last_row;
-assign last_row=(row_cnt==7'b1111110);
-
-//assign res_addr={core_cnt[13:7]+(status!=JOIN_UP) ,core_cnt[6:0]}+1;
-
+logic [1:0]loop_cnt;
 /*
 core(2*3):
 |0,0|1,0|2,0|
@@ -56,102 +51,83 @@ BOTTOM:
 always_ff @(posedge clk,negedge reset) begin
 	if(!reset) begin
 		done<=0;
-		sti_addr<=10'd8;
-		sti_rd<=0;
-		tmp_cnt<=0;
+		tmp_cnt<=1;
 		sti_cnt<=0;
 		row_cnt<=0;
-		req_cnt<=0;
+		sti_rd<=0;
+		req_cnt<=1;
 		res_addr<=0;
 		res_wr<=0;
 		res_rd<=0;
 		res_do<=0;
 		sti_tmp<='{default:'0};
-		status<=INIT;
+		status<=GET_STI_DATA;
 	end
 	else begin
 		case(status)
-			INIT:begin
+			GET_STI_DATA:begin
 				if(sti_rd) begin
-					for(i=2;i<18;++i) sti_tmp[1][i]<=sti_di[17-i];
-					sti_rd<=0;
-					tmp_cnt<=1;
-					status<=JOIN_UP;
+					for(i=2;i<18;++i) sti_tmp[req_cnt][i]<={6'd0,sti_di[17-i]};
+					if(req_cnt) status<=JOIN_UP;
 				end
-				else sti_rd<=1;
+				{sti_rd,req_cnt}<={sti_rd,req_cnt}+1;
+				sti_addr<={row_cnt+(!req_cnt),sti_cnt};
 			end
 			JOIN_UP:begin
-				if
-			end
-		endcase
-		/*
-		case(status)
-			INIT:begin
-				if(sti_rd) begin
-					for(i=2;i<18;++i) sti_tmp[1][i]<=sti_di[17-i];
-					sti_rd<=0;
-					status<=JOIN_UP;
-				end
-				else sti_rd<=1;
-			end
-			JOIN_UP:begin
-				tmp=minimum('{`P(0,1),`P(0,0),`P(1,0),`P(2,0),8'hff})+1;
 				res_wr<=`P(1,1);
-				`P(1,1)<=`P(1,1)? tmp:0;
-				res_addr<={row_cnt+1,sti_cnt,tmp_cnt}-1;
-				res_do<=tmp;
-				res_rd<=0;
-				status<=CTRL;
-			end
-			CTRL:begin
-				res_wr<=0;
-				if(~tmp_cnt) begin
-					res_wr<=0;
-					tmp_cnt<=tmp_cnt+1;
-					status<=JOIN_BOTTOM;
-				end
-				else begin //end col of sti_tmp
-					if(last_row && sti_cnt==3'b111) status<=END;
-					else begin
-						if(sti_rd) begin
-							if(res_rd) begin
-								res_rd<=0;
-								for(i=0;i<18;++i) sti_tmp[0][i]<=(last_row)? 0:sti_tmp[1][i];
-								sti_tmp[1][0]<=res_di;
-								for(i=2;i<18;++i) sti_tmp[1][i]<=sti_di[17-i];
-								sti_addr<=sti_cnt? {row_cnt+2,sti_cnt}-1:0;
+				res_addr<={row_cnt+7'd1,sti_cnt,tmp_cnt}-1;
+				res_di<=minimum(`P(0,0),`P(1,0),`P(2,0),`P(0,1))+1;
+				if(tmp_cnt==4'b1111) begin
+					if(sti_cnt==3'b111) begin
+						if(row_cnt==7'b1111101) begin
+							for(loop_cnt=0;loop_cnt<3;++loop_cnt) begin
+								sti_tmp[0][loop_cnt]<=sti_tmp[0][15+loop_cnt];
+								sti_tmp[1][loop_cnt]<=sti_tmp[1][15+loop_cnt];
 							end
-							else begin
-								sti_tmp[1][1]<=sti_di[0];
-								//finish request data
-								sti_rd<=0;
-								tmp_cnt<=0;
-								row_cnt<=(last_row)? 0:row_cnt+1;
-								sti_cnt<=sti_cnt+last_row;
-								status<=JOIN_BOTTOM;
-							end
+							status<=JOIN_BOTTOM;
 						end
-						else begin //start request data
-							sti_rd<=1;
-							sti_addr<=last_row? {7'd1,sti_cnt}+1:{row_cnt+2,sti_cnt};
-							res_rd<=1;
-							res_addr<=last_row? {7'd1,sti_cnt,4'he}:sti_cnt? {row_cnt+7'd2,sti_cnt-3'd1,4'he}:0;
+						else begin
+							{row_cnt,sti_cnt,tmp_cnt}<={row_cnt,sti_cnt,tmp_cnt}+3;
+							for(loop_cnt=0;loop_cnt<4;++loop_cnt) sti_tmp[loop_cnt[1]][loop_cnt[0]]<=0;
+							req_cnt<=1;
+							status<=GET_STI_DATA;
 						end
 					end
+					else begin
+						{sti_cnt,tmp_cnt}<={sti_cnt,tmp_cnt}+1;
+						for(loop_cnt=0;loop_cnt<4;++loop_cnt) sti_tmp[loop_cnt[1]][loop_cnt[0]]<=sti_tmp[loop_cnt[1]][16+loop_cnt[0]];
+						req_cnt<=1;
+						status<=GET_STI_DATA;
+					end
 				end
+				else tmp_cnt<=tmp_cnt+1;
 			end
 			JOIN_BOTTOM:begin
-				tmp=minimum('{`P(2,1)+1,`P(0,1)+1,`P(1,1)+1,`P(2,1)+1,`P(1,0)});
-				res_wr<=`P(1,0);
-				`P(1,0)<=`P(1,0)? tmp:0;
-				res_addr<={row_cnt,sti_cnt,tmp_cnt}-1;
-				res_do<=tmp;
 				res_rd<=0;
-				status<=CTRL;
+				res_wr<=(sti_tmp[1][0])!=0);
+				res_addr<={row_cnt,sti_cnt,tmp_cnt}-1;
+				tmp=minimum(sti_tmp[0][1],sti_tmp[1][1],sti_tmp[2][1],sti_tmp[2][0])+1;
+				res_di<=tmp<sti_tmp[1][0]? tmp:sti_tmp[1][0];
+				status<=GET_RES_DATA;
+				//prepare next data
+				for(loop_cnt=0;loop_cnt<4;++loop_cnt) sti_tmp[loop_cnt[1]][loop_cnt[0]]<=sti_tmp[loop_cnt[1]][1+loop_cnt[0]];
+				{row_cnt,sti_cnt,tmp_cnt}<={row_cnt,sti_cnt,tmp_cnt}-(({sti_cnt,tmp_cnt}==7'b0000001)? 3:1);
+				{res_rd,req_cnt}<={0,1};
+				req_cnt2<=({sti_cnt,tmp_cnt}==7'b0000001)? 3:1;
 			end
-			END: done<=1;
+			GET_RES_DATA:begin
+				if(res_rd) begin
+					sti_tmp[0][req_cnt]<=res_di;
+					if(req_cnt) begin 
+						if(req_cnt2) {row_cnt,sti_cnt,tmp_cnt}<={row_cnt,sti_cnt,tmp_cnt}-1;
+						else status<=JOIN_BOTTOM;
+					end
+				end
+				res_rd<=1;
+				req_cnt<=!req_cnt;
+				res_addr<={row_cnt+(!req_cnt),sti_cnt,tmp_cnt}-3;
+			end
 		endcase
-		*/
 	end
 end
 endmodule
