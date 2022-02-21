1a119b33f44a13fc1d73780cb568aadb81b673b2
cicero884 <joy861225@fmail.com>
2022-02-21 18:43:04 +0800

    add 2016(level A,score=5,295,560,925)

--- graduate/icc2016cb_pre/sim/testfixture.v
+++ graduate/icc2016cb_pre/sim/testfixture.v
@@ -1,5 +1,5 @@
 `timescale 1ns/10ps
-`define CYCLE      12.5          	  // Modify your clock period here
+`define CYCLE      9          	  // Modify your clock period here
 `define SDFFILE    "./SYN/LBP_syn.sdf"	  // Modify your sdf file name
 `define End_CYCLE  100000000000              // Modify cycle times once your design need more cycle times!
 
@@ -50,7 +50,7 @@ always begin #(`CYCLE/2) clk = ~clk; end
 
 initial begin
 	$fsdbDumpfile("LBP.fsdb");
-	$fsdbDumpvars;
+	$fsdbDumpvars("+all");
 end
 
 initial begin  // data input
