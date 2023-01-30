734e1534bade5dfeace3f7a7dc9167ee054f991c
cicero884 <joy861225@gmail.com>
2023-01-30 18:18:52 +0800

    Add 2022cb_pre and fix makefile for dic env

---
+++ graduate/icc2022cb_pre/2022/JAM.v
@@ -0,0 +1,14 @@
+module JAM (
+input CLK,
+input RST,
+output reg [2:0] W,
+output reg [2:0] J,
+input [6:0] Cost,
+output reg [3:0] MatchCount,
+output reg [9:0] MinCost,
+output reg Valid );
+
+
+endmodule
+
+
