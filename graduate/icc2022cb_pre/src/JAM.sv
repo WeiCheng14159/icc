57650259f9eb02e6a727c4736dd86e70ed9d06f2
cicero884 <joy861225@gmail.com>
2023-02-10 16:42:11 +0800

    try fix to syn

--- graduate/icc2022cb_pre/src/JAM.sv
+++ graduate/icc2022cb_pre/src/JAM.sv
@@ -2,7 +2,6 @@
 	temp=a;\
 	a=b;\
 	b=temp;
-
 module JAM (
 	input CLK,
 	input RST,
@@ -11,47 +10,47 @@ module JAM (
 	input [6:0] Cost,
 	output logic [3:0] MatchCount,
 	output logic [9:0] MinCost,
-	output logic Valid
-);
+output logic Valid );
 
-logic [6:0]cost[8][8],cur_cost[8];
-logic [6:0]swap_point,larger_point;
-logic [2:0]order[8],sp_number,lp_number;
+logic [6:0]cost[8][8];
+logic [2:0]order[8];
 logic [2:0]new_order[8];
 enum {INIT,CALC,DONE}status;
 
 logic [3:0]temp; // for macro
 
-logic [2:0]min_point,pw,pj;
+logic [2:0]swap_point,min_point,pw,pj;
+logic [3:0]i,j;
+logic [9:0]tmp_cost;
 
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
 		for(i=0;i<8;++i) order[i]<=i;
-		cost<='{default: 0};
+		cost<='{default: '0};
 		W<=3'd7;
 		J<=3'd7;
 		pw<=3'd7;
 		pj<=3'd7;
-		cmp<=8'd0;
 		MatchCount<=4'd0;
 		MinCost<={10{1'b1}};
 		Valid<=1'b0;
 	end
 	else begin
-		unique case(status)
+		case(status)
 			INIT: begin
 				if(!W) begin
-					if(J) J<=J-3'd1;
+					if(!J) status<=CALC;
+					else J<=J-3'd1;
 					W<=3'd7;
 				end
 				else W<=W-3'd1;
 				pw<=W;
 				pj<=J;
-				if(!{pw,pj}) status<=CALC;
 				cost[pw][pj]<=Cost;
 			end
 			CALC: begin
-				//find swap point
+				//swap
+				for(i=0;i<8;++i) new_order[i]=order[i];
 				swap_point=0;
 				for(i=7;i;--i) begin
 					if(order[i]>order[i-1]) begin
@@ -59,15 +58,19 @@ always_ff@(posedge CLK,posedge RST) begin
 						break;
 					end
 				end
-				j=swap_point;
-				for(i=1;i<8;++i) begin
-					if(i>=swap_point&&(order[i] inside {[order[swap_point-1]:order[j]]})) j=i;
+				for(i=swap_point+1,j=swap_point;i<8;++i) begin
+					if(order[j]>order[i]&&order[i]>order[swap_point-1]) j=i;
 				end
 				`SWAP(new_order[j],new_order[swap_point-1]);
-				for(i=swap_point,j=7;i<j;++i,--j) begin
-					`SWAP(new_order[i],new_order[j])
-				end
-				for(i=0;i<8;++i) order[i]<=new_order[i];
+				unique case(swap_point)
+					3'd1: order<='{new_order[0],new_order[1],new_order[2],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
+					3'd2: order<='{new_order[1],new_order[0],new_order[2],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
+					3'd3: order<='{new_order[2],new_order[1],new_order[0],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
+					3'd4: order<='{new_order[3],new_order[2],new_order[1],new_order[0],new_order[4],new_order[5],new_order[6],new_order[7]};
+					3'd5: order<='{new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[5],new_order[6],new_order[7]};
+					3'd6: order<='{new_order[5],new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[6],new_order[7]};
+					3'd7: order<='{new_order[6],new_order[5],new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[7]};
+				endcase
 				if(!swap_point) status<=DONE;
 				//calc
 				tmp_cost=0;
@@ -87,5 +90,3 @@ end
 
 
 endmodule
-
-
