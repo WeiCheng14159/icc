efd03966703db3cf8606b8a1e17babae4fde3a8d
cicero884 <joy861225@gmail.com>
2023-02-13 15:56:07 +0800

    try new 2022(duel pipeline)

--- graduate/icc2022cb_pre/src/JAM.sv
+++ graduate/icc2022cb_pre/src/JAM.sv
@@ -2,6 +2,7 @@
 	temp=a;\
 	a=b;\
 	b=temp;
+
 module JAM (
 	input CLK,
 	input RST,
@@ -13,27 +14,81 @@ module JAM (
 output logic Valid );
 
 logic [6:0]cost[8][8];
-logic [2:0]order[8];
-logic [2:0]new_order[8];
-enum {INIT,CALC,DONE}status;
+logic [2:0]order[2][8],tmp_order[8];
+enum {INIT,GET,CALC,DONE}status_e;
 
 logic [3:0]temp; // for macro
 
-logic [2:0]swap_point,min_point,pw,pj;
+logic [2:0]swap_index[2],larger_index[2];
+logic [2:0]pw,pj,cnt,swap_number[2],larger_number[2];
 logic [3:0]i,j;
-logic [9:0]tmp_cost;
+logic [9:0]cur_sum;
+
+//'{order[0],order[1]....,order[7]}
+//find swap point and min point
+always_ff@(posedge CLK,posedge RST) begin
+	if(RST) begin
+		swap_index<='{default:'0};
+		swap_number<='{default:'0};
+	end
+	else begin
+		if(cnt) begin
+		end
+		else begin
+			if(cnt) begin
+				if(order[0][cnt]>order[0][cnt-1]) begin
+					swap_index[0]<=cnt-1;
+					swap_number[0]<=order[0][cnt-1];
+					larger_index[0]<=0;
+					larger_number[0]<=3'd7
+				end
+				else if(order[0][cnt] inside [swap_number:larger_number]) begin
+					larger_index<=cnt;
+					larger_number<=order[0][cnt];
+				end
+			end
+			else begin
+				swap_index<=3'd0;
+				swap_index[1]<=swap_index[0];
+				swap_number[1]<=swap_number[0];
+				larger_index[1]<=larger_index[0];
+				larger_number[1]<=larger_number[0];
+			end
+		end
+	end
+end
+//sum & check
+always_ff@(posedge CLK,posedge RST) begin
+	if(RST) begin
+		cur_sum<={10{1'b1}};
+		MatchCount<=4'd0;
+		MinCost<={10{1'b1}};
+	end
+	else begin
+		if(cnt==0) begin
+			if(cur_sum<MinCost) begin
+				MinCost<=cur_sum;
+				MatchCount<=1;
+			end
+			else if(cur_sum==MinCost) MatchCount<=MatchCount+1;
+			cur_sum<=Cost[1][order[1][0]];
+		end
+		else cur_sum<=cur_sum+Cost[cnt][order[0][cnt]];
+	end
+end
 
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
-		for(i=0;i<8;++i) order[i]<=i;
+		order[0]<='{3'd0,3'd1,3'd2,3'd3,3'd4,3'd5'3'd6,3'd7};
+		order[1]<='{3'd4,3'd0,3'd1,3'd2,3'd3,3'd5,3'd6,3'd7};
 		cost<='{default: '0};
 		W<=3'd7;
 		J<=3'd7;
 		pw<=3'd7;
 		pj<=3'd7;
-		MatchCount<=4'd0;
-		MinCost<={10{1'b1}};
 		Valid<=1'b0;
+		status<=INIT;
+		cnt<=3'd0;
 	end
 	else begin
 		case(status)
@@ -49,37 +104,23 @@ always_ff@(posedge CLK,posedge RST) begin
 				cost[pw][pj]<=Cost;
 			end
 			CALC: begin
-				//swap
-				for(i=0;i<8;++i) new_order[i]=order[i];
-				swap_point=0;
-				for(i=7;i;--i) begin
-					if(order[i]>order[i-1]) begin
-						swap_point=i;
-						break;
+				cnt<=cnt+3'd1;
+				// wonder how to deal with data confusion while writing?
+				//swap points
+				if(cnt==0) begin
+					if(!swap_index) status<=done;
+					tmp_order=order[0]
+					order[0]<=order[1];
+					tmp_order[swap_index[0]]=larger_number;
+					tmp_order[larger_index[0]]=swap_number;
+					order[1]<=tmp_order;
+				end
+				else begin
+					if(cnt inside [swap_index[1]+1:3+((cnt+1)>>1)]) begin
+						order[1][cnt]<=order[1][3'd8-cnt+swap_index[1]];
+						order[1][3'd8-cnt+swap_index[1]]<=order[1][cnt];
 					end
 				end
-				for(i=swap_point+1,j=swap_point;i<8;++i) begin
-					if(order[j]>order[i]&&order[i]>order[swap_point-1]) j=i;
-				end
-				`SWAP(new_order[j],new_order[swap_point-1]);
-				unique case(swap_point)
-					3'd1: order<='{new_order[0],new_order[1],new_order[2],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
-					3'd2: order<='{new_order[1],new_order[0],new_order[2],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
-					3'd3: order<='{new_order[2],new_order[1],new_order[0],new_order[3],new_order[4],new_order[5],new_order[6],new_order[7]};
-					3'd4: order<='{new_order[3],new_order[2],new_order[1],new_order[0],new_order[4],new_order[5],new_order[6],new_order[7]};
-					3'd5: order<='{new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[5],new_order[6],new_order[7]};
-					3'd6: order<='{new_order[5],new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[6],new_order[7]};
-					3'd7: order<='{new_order[6],new_order[5],new_order[4],new_order[3],new_order[2],new_order[1],new_order[0],new_order[7]};
-				endcase
-				if(!swap_point) status<=DONE;
-				//calc
-				tmp_cost=0;
-				for(i=0;i<8;++i) tmp_cost=tmp_cost+cost[i][order[i]];
-				if(tmp_cost<MinCost) begin
-					MinCost<=tmp_cost;
-					MatchCount<=4'd1;
-				end
-				else if(tmp_cost==MinCost) MatchCount<=MatchCount+1;
 			end
 			DONE: begin
 				Valid<=1;
