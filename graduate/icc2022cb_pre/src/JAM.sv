bc3b5df3d03cd31723d7b3214940777f90f02f08
cicero884 <joy861225@gmail.com>
2023-02-13 18:46:56 +0800

    Fix 2022 pass rtl

--- graduate/icc2022cb_pre/src/JAM.sv
+++ graduate/icc2022cb_pre/src/JAM.sv
@@ -28,32 +28,30 @@ logic [9:0]cur_sum;
 //find swap point and min point
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
-		swap_index<='{default:'0};
-		swap_number<='{default:'0};
+		swap_index<='{3'd6,3'd0};
+		swap_number<='{3'd6,3'd0};
+		larger_index<='{3'd7,3'd0};
+		larger_number<='{3'd7,3'd0};
 	end
 	else begin
 		if(cnt) begin
+			if(order[0][cnt]>order[0][cnt-1]) begin
+				swap_index[0]<=cnt-1;
+				swap_number[0]<=order[0][cnt-1];
+				larger_index[0]<=cnt;
+				larger_number[0]<=order[0][cnt];
+			end
+			else if(order[0][cnt] inside {[swap_number[0]:larger_number[0]]}) begin
+				larger_index[0]<=cnt;
+				larger_number[0]<=order[0][cnt];
+			end
 		end
 		else begin
-			if(cnt) begin
-				if(order[0][cnt]>order[0][cnt-1]) begin
-					swap_index[0]<=cnt-1;
-					swap_number[0]<=order[0][cnt-1];
-					larger_index[0]<=0;
-					larger_number[0]<=3'd7
-				end
-				else if(order[0][cnt] inside [swap_number:larger_number]) begin
-					larger_index<=cnt;
-					larger_number<=order[0][cnt];
-				end
-			end
-			else begin
-				swap_index<=3'd0;
-				swap_index[1]<=swap_index[0];
-				swap_number[1]<=swap_number[0];
-				larger_index[1]<=larger_index[0];
-				larger_number[1]<=larger_number[0];
-			end
+			if(status_e==CALC) swap_index[0]<=3'd7;
+			swap_index[1]<=swap_index[0];
+			swap_number[1]<=swap_number[0];
+			larger_index[1]<=larger_index[0];
+			larger_number[1]<=larger_number[0];
 		end
 	end
 end
@@ -65,21 +63,21 @@ always_ff@(posedge CLK,posedge RST) begin
 		MinCost<={10{1'b1}};
 	end
 	else begin
-		if(cnt==0) begin
+		if(cnt==0&&status_e==CALC) begin
 			if(cur_sum<MinCost) begin
 				MinCost<=cur_sum;
 				MatchCount<=1;
 			end
 			else if(cur_sum==MinCost) MatchCount<=MatchCount+1;
-			cur_sum<=Cost[1][order[1][0]];
+			cur_sum<=cost[0][order[1][0]];
 		end
-		else cur_sum<=cur_sum+Cost[cnt][order[0][cnt]];
+		else cur_sum<=cur_sum+cost[cnt][order[0][cnt]];
 	end
 end
 
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
-		order[0]<='{3'd0,3'd1,3'd2,3'd3,3'd4,3'd5'3'd6,3'd7};
+		order[0]<='{3'd0,3'd1,3'd2,3'd3,3'd4,3'd5,3'd6,3'd7};
 		order[1]<='{3'd4,3'd0,3'd1,3'd2,3'd3,3'd5,3'd6,3'd7};
 		cost<='{default: '0};
 		W<=3'd7;
@@ -87,20 +85,20 @@ always_ff@(posedge CLK,posedge RST) begin
 		pw<=3'd7;
 		pj<=3'd7;
 		Valid<=1'b0;
-		status<=INIT;
+		status_e<=INIT;
 		cnt<=3'd0;
 	end
 	else begin
-		case(status)
+		case(status_e)
 			INIT: begin
-				if(!W) begin
-					if(!J) status<=CALC;
-					else J<=J-3'd1;
+				if(W) W<=W-3'd1;
+				else begin
+					J<=J-3'd1;
 					W<=3'd7;
 				end
-				else W<=W-3'd1;
 				pw<=W;
 				pj<=J;
+				if(!(pw|pj)) status_e<=CALC;
 				cost[pw][pj]<=Cost;
 			end
 			CALC: begin
@@ -108,17 +106,17 @@ always_ff@(posedge CLK,posedge RST) begin
 				// wonder how to deal with data confusion while writing?
 				//swap points
 				if(cnt==0) begin
-					if(!swap_index) status<=done;
-					tmp_order=order[0]
+					if(swap_index[0]==3'd7) status_e<=DONE;
+					tmp_order=order[0];
 					order[0]<=order[1];
-					tmp_order[swap_index[0]]=larger_number;
-					tmp_order[larger_index[0]]=swap_number;
+					tmp_order[swap_index[0]]=larger_number[0];
+					tmp_order[larger_index[0]]=swap_number[0];
 					order[1]<=tmp_order;
 				end
 				else begin
-					if(cnt inside [swap_index[1]+1:3+((cnt+1)>>1)]) begin
-						order[1][cnt]<=order[1][3'd8-cnt+swap_index[1]];
-						order[1][3'd8-cnt+swap_index[1]]<=order[1][cnt];
+					if(cnt inside {[swap_index[1]+1:3+((swap_index[1]+1)>>1)]}) begin
+						order[1][cnt]<=order[1][4'd8-cnt+swap_index[1]];
+						order[1][4'd8-cnt+swap_index[1]]<=order[1][cnt];
 					end
 				end
 			end
