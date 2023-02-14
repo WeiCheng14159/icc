806ef878e9390a0efcb50bfd740d8fa28102ef1a
cicero884 <joy861225@gmail.com>
2023-02-14 18:08:59 +0800

    Add 2022 grade A version
    area:4983.566365
    timing:3628875 NS

--- graduate/icc2022cb_pre/script/synthesize.tcl
+++ graduate/icc2022cb_pre/script/synthesize.tcl
@@ -1,53 +1,24 @@
-# Force multicore
-set_host_options -max_cores 16
-
-# Read all Files
-set top JAM
-#read_verilog ../src/${top}.v
-read_file -autoread -top ${top} -recursive {../src} -library ${top}
-current_design ${top}
+#Read All Files
+#read_file -format verilog  JAM.v
+read_file -format sverilog  ../src/JAM.sv
+current_design JAM
 link
 
-# Setting Clock Constraits
-source -echo -verbose ../script/${top}.sdc
-
-# High fanout threshold
-# set high_fanout_net_threshold 0
-report_net_fanout -high_fanout
-
+#Setting Clock Constraints
+source -echo -verbose JAM.sdc
+check_design
+set high_fanout_net_threshold 0
 uniquify
 set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
- 
-set_structure -timing true
- 
-check_design
 
-# Synthesize (ultimate)
-compile_ultra -no_autoungroup -no_boundary_optimization 
-#compie -retime -gate_clock -map_effort high -area_effort high
-compile_ultra -inc
-#compile -map_effort high -area_effort high -inc
-#compile_ultra
+#Synthesis all design
+compile -map_effort high -area_effort high
+compile -map_effort high -area_effort high -inc
+compile_ultra
 
-current_design [get_designs ${top}]
- 
-remove_unconnected_ports -blast_buses [get_cells -hierarchical *]
- 
-set bus_inference_style {%s[%d]}
-set bus_naming_style {%s[%d]}
-set hdlout_internal_busses true
-change_names -hierarchy -rule verilog
-define_name_rules name_rule -allowed {a-z A-Z 0-9 _}   -max_length 255 -type cell
-define_name_rules name_rule -allowed {a-z A-Z 0-9 _[]} -max_length 255 -type net
-define_name_rules name_rule -map {{"\\*cell\\*" "cell"}}
-define_name_rules name_rule -case_insensitive
-change_names -hierarchy -rules name_rule
-
-write -format ddc  -hierarchy -output "${top}_syn.ddc"
-write_sdf ../syn/${top}_syn.sdf
-write_file -format verilog -hierarchy -output ../syn/${top}_syn.v
+write -format ddc     -hierarchy -output "JAM_syn.ddc"
+write_sdf -version 1.0  ../syn/JAM_syn.sdf
+write -format verilog -hierarchy -output ../syn/JAM_syn.v
 report_area > area.log
 report_timing > timing.log
-report_qor > ${top}_syn.qor
-
-# exit
+report_qor   >  JAM_syn.qor
--- graduate/icc2022cb_pre/src/JAM.sv
+++ graduate/icc2022cb_pre/src/JAM.sv
@@ -1,8 +1,3 @@
-`define SWAP(a,b) \
-	temp=a;\
-	a=b;\
-	b=temp;
-
 module JAM (
 	input CLK,
 	input RST,
@@ -13,112 +8,109 @@ module JAM (
 	output logic [9:0] MinCost,
 output logic Valid );
 
-logic [6:0]cost[8][8];
-logic [2:0]order[8],tmp_order[8];
-enum {INIT,GET,CALC,DONE}status_e;
-
-logic [3:0]temp; // for macro
+logic [2:0]order[8],tmp_order[8],rotate[8];
+enum {CALC,CHECK,DONE,NONE}status_e[3];
 
 logic [2:0]swap_index,larger_index;
-logic [2:0]pw,pj,cnt,swap_number,larger_number;
-logic [3:0]i,j;
+logic [2:0]cnt,swap_number,larger_number;
 logic [9:0]cur_sum;
+logic init_skip;
 
-//'{order[0],order[1]....,order[7]}
 //find swap point and min point
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
-		swap_index<=3'd0;
-		swap_number<=3'd0;
-		larger_index<=3'd0;
-		larger_number<=3'd0;
+		swap_index<=3'd6;
+		swap_number<=3'd6;
+		larger_index<=3'd6;
+		larger_number<=3'd6;
 	end
 	else begin
-		if(cnt==7) swap_index<=3'd7;
-		else if(order[cnt+1]>order[cnt]) begin
-			swap_index<=cnt;
-			swap_number<=order[cnt];
-		end
-		if(order[cnt] inside {[swap_number:larger_number]}) begin
-			larger_index<=cnt;
-			larger_number<=order[cnt];
-		end
-	end
-end
-//sum & check
-//logic [9:0]adder_sum;
-//assign adder_sum=cur_sum+cost[cnt][order[cnt]];
-always_ff@(posedge CLK,posedge RST) begin
-	if(RST) begin
-		cur_sum<={10{1'b1}};
-		MatchCount<=4'd0;
-		MinCost<={10{1'b1}};
-	end
-	else begin
-		if(cnt==7) begin
-			if(cur_sum+cost[7][order[7]]<MinCost) begin
-				MinCost<=cur_sum+cost[7][order[7]];
-				MatchCount<=1;
+		case(status_e[0])
+			CALC: begin
+				if(cnt!=3'd7 && order[1]>order[0]) begin
+					swap_index<=cnt;
+					swap_number<=order[0];
+					larger_index<=cnt;
+					larger_number<=order[1];
+				end
+				else if(order[0] inside {[swap_number:larger_number]}) begin
+					larger_index<=cnt;
+					larger_number<=order[0];
+				end
 			end
-			else if(cur_sum==MinCost) MatchCount<=MatchCount+1;
-			cur_sum<=0;
-		end
-		else cur_sum<=cur_sum+cost[cnt][order[cnt]];
+			CHECK: swap_index<=3'd7;
+		endcase
 	end
 end
 
-//reorder order
-always_comb begin
-	tmp_order=order;
-	tmp_order[swap_index]=larger_number;
-	tmp_order[larger_index]=swap_number;
-	unique case(swap_index)
-		3'd0: tmp_order<='{tmp_order[0],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3],tmp_order[2],tmp_order[1]};
-		3'd1: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3],tmp_order[2]};
-		3'd2: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3]};
-		3'd3: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4]};
-		3'd4: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[7],tmp_order[6],tmp_order[5]};
-		3'd5: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[5],tmp_order[7],tmp_order[6]};
-		3'd6: tmp_order<='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[5],tmp_order[6],tmp_order[7]};
-	endcase
+//sum & check
+always_ff@(posedge CLK,posedge RST) begin
+	if(RST) begin
+		cur_sum<=0;
+		W<=0;
+		J<=0;
+		MatchCount<=4'd0;
+		MinCost<={10{1'b1}};
+		init_skip<=1;
+	end
+	else begin
+		if(status_e[0]==CALC) begin
+			W<=cnt;
+			J<=order[0];
+		end
+		case (status_e[2])
+			CALC: cur_sum<=cur_sum+Cost;
+			CHECK: begin
+				if(cur_sum<MinCost) begin
+					MinCost<=cur_sum;
+					MatchCount<=1;
+				end
+				else if(cur_sum==MinCost) MatchCount<=MatchCount+1;
+				cur_sum<=0;
+			end
+		endcase
+	end
 end
+
+assign rotate='{order[1],order[2],order[3],order[4],order[5],order[6],order[7],order[0]};
 
 always_ff@(posedge CLK,posedge RST) begin
 	if(RST) begin
 		order<='{3'd0,3'd1,3'd2,3'd3,3'd4,3'd5,3'd6,3'd7};
-		cost<='{default: '0};
-		W<=3'd7;
-		J<=3'd7;
-		pw<=3'd7;
-		pj<=3'd7;
 		Valid<=1'b0;
-		status_e<=INIT;
+		status_e<='{CALC,NONE,NONE};
 		cnt<=3'd0;
 	end
 	else begin
-		case(status_e)
-			INIT: begin
-				if(W) W<=W-3'd1;
-				else begin
-					J<=J-3'd1;
-					W<=3'd7;
-				end
-				pw<=W;
-				pj<=J;
-				if(!(pw|pj)) status_e<=CALC;
-				cost[pw][pj]<=Cost;
-			end
+		for(int i=1;i<3;++i) status_e[i]<=status_e[i-1];
+		case(status_e[0])
 			CALC: begin
 				cnt<=cnt+3'd1;
-				// wonder how to deal with data confusion while writing?
-				//swap points
-				if(cnt==7) begin
-					if(swap_index==3'd7) status_e<=DONE;
-					order<=tmp_order;
-				end
+				if(cnt==3'd7) status_e[0]<=CHECK;
+				order<=rotate;
+
+			end
+			CHECK: begin
+				//reorder order
+				tmp_order=order;
+				tmp_order[swap_index]=larger_number;
+				tmp_order[larger_index]=swap_number;
+				case(swap_index)
+					3'd0: tmp_order='{tmp_order[0],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3],tmp_order[2],tmp_order[1]};
+					3'd1: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3],tmp_order[2]};
+					3'd2: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4],tmp_order[3]};
+					3'd3: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[7],tmp_order[6],tmp_order[5],tmp_order[4]};
+					3'd4: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[7],tmp_order[6],tmp_order[5]};
+					3'd5: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[5],tmp_order[7],tmp_order[6]};
+					3'd6: tmp_order='{tmp_order[0],tmp_order[1],tmp_order[2],tmp_order[3],tmp_order[4],tmp_order[5],tmp_order[6],tmp_order[7]};
+				endcase
+				order<=tmp_order;
+
+				if(swap_index==3'd7) status_e[0]<=DONE;
+				else status_e[0]<=CALC;
 			end
 			DONE: begin
-				Valid<=1;
+				if(status_e[2]==DONE) Valid<=1;
 			end
 		endcase
 	end
